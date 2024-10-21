98d9884c6a52d2d87becb617a310f9fc92ce738d
MohamedTrabelsi1 <trabelsi.mohamed@esprit.tn>
2024-10-21 21:33:25 +0100

    Projet V1

---
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -0,0 +1,22 @@
+package tn.esprit.tpfoyer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAspectJAutoProxy
+@EnableJpaRepositories
+public class TpFoyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpFoyerApplication.class, args);
+    }
+
+}
