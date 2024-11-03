2f0370c7bf163bca2c77405407f3ac8b4dcf2295
BedouiMehdi <mahdi.bedoui@esprit.tn>
2024-11-03 17:21:09 +0100

    testtest

--- src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class EtudiantRestController {
 
     IEtudiantService etudiantService;
-
+    //testtest
 
     @GetMapping("/retrieve-all-etudiants")
     public List<Etudiant> getEtudiants() {
