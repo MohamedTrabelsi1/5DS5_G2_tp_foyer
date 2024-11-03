14d5eb831e224b436364977ad1313059932f4cdb
BedouiMehdi <mahdi.bedoui@esprit.tn>
2024-11-03 19:11:15 +0100

    updated jenkin

--- src/test/java/tn/esprit/tpfoyer/EtudiantRestControllerTest.java
+++ src/test/java/tn/esprit/tpfoyer/EtudiantRestControllerTest.java
@@ -1,6 +1,7 @@
 package tn.esprit.tpfoyer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,6 +18,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(EtudiantRestController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class EtudiantRestControllerTest {
 
     @Autowired
