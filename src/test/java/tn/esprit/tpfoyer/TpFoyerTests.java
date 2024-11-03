55e46df4ff4399edc849c6f8a23ca1569b19a240
BedouiMehdi <mahdi.bedoui@esprit.tn>
2024-11-03 20:27:39 +0100

    updated jenkins

--- src/test/java/tn/esprit/tpfoyer/TpFoyerTests.java
+++ src/test/java/tn/esprit/tpfoyer/TpFoyerTests.java
@@ -2,8 +2,9 @@ package tn.esprit.tpfoyer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.tpfoyer.entity.Etudiant;
+import tn.esprit.tpfoyer.service.EtudiantServiceImpl;
 
-@SpringBootTest(classes = Etudiant.class)
+@SpringBootTest(classes = EtudiantServiceImpl.class)
 public class TpFoyerTests {
     @Test
     void contextLoads(){
