83219e8384c46f380a8ef72e66078b9a1205e7af
Ayoub CHAAIRIA <ayoub.chaairia@esprit.tn>
2024-11-06 00:33:52 +0100

    TestUnitaire

---
+++ src/test/java/tn/esprit/spring/foyer/FoyerServiceImplTest.java
@@ -0,0 +1,85 @@
+package tn.esprit.spring.foyer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.tpfoyer.entity.Foyer;
+import tn.esprit.tpfoyer.repository.FoyerRepository;
+import tn.esprit.tpfoyer.service.FoyerServiceImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class FoyerServiceImplTest {
+
+    @Mock
+    private FoyerRepository foyerRepository;
+
+    @InjectMocks
+    private FoyerServiceImpl foyerService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testRetrieveAllFoyers() {
+        List<Foyer> mockFoyers = new ArrayList<>();
+        when(foyerRepository.findAll()).thenReturn(mockFoyers);
+
+        List<Foyer> result = foyerService.retrieveAllFoyers();
+
+        assertEquals(mockFoyers, result);
+        verify(foyerRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testRetrieveFoyer() {
+        Long foyerId = 1L;
+        Foyer mockFoyer = new Foyer();
+        when(foyerRepository.findById(foyerId)).thenReturn(Optional.of(mockFoyer));
+
+        Foyer result = foyerService.retrieveFoyer(foyerId);
+
+        assertEquals(mockFoyer, result);
+        verify(foyerRepository, times(1)).findById(foyerId);
+    }
+
+    @Test
+    void testAddFoyer() {
+        Foyer foyer = new Foyer();
+        when(foyerRepository.save(foyer)).thenReturn(foyer);
+
+        Foyer result = foyerService.addFoyer(foyer);
+
+        assertEquals(foyer, result);
+        verify(foyerRepository, times(1)).save(foyer);
+    }
+
+    @Test
+    void testModifyFoyer() {
+        Foyer foyer = new Foyer();
+        when(foyerRepository.save(foyer)).thenReturn(foyer);
+
+        Foyer result = foyerService.modifyFoyer(foyer);
+
+        assertEquals(foyer, result);
+        verify(foyerRepository, times(1)).save(foyer);
+    }
+
+    @Test
+    void testRemoveFoyer() {
+        Long foyerId = 1L;
+
+        foyerService.removeFoyer(foyerId);
+
+        verify(foyerRepository, times(1)).deleteById(foyerId);
+    }
+}
